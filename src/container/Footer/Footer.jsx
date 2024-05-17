9e88bebf494d664e99f96b333b00a773ab6a6387
tvto-ipc <144618758+tvto-ipc@users.noreply.github.com>
2024-05-17 16:10:38 -0400

    project updates

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -10,6 +10,38 @@ const Footer = () => (
     <FooterOverlay />
     <Newsletter />
 
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">123 Main Street, Pleasant Hill, CA 90210</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">09:00 am - 03:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">09:00 am - 04:00 am</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2024 Gericht. All Rights reserved.</p>
+    </div>
+
   </div>
 );
 
